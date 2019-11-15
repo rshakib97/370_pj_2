31384b4f06bca81fce6947b7e8b4dc6d9e5a8e08
Adam Carballido <carballido.adam@yahoo.com>
2019-11-15 12:43:54 -0500

    Added uneeded import

--- src/MainWindow.java
+++ src/MainWindow.java
@@ -13,7 +13,6 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Pane;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
